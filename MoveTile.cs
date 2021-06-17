92071dacfe2809e9b3494aea3c4ae6f5323ba1db
Jaune <jaunelacouleur@gmail.com>
2021-06-17 09:28:23 +0200

    feat: C# 8.0

--- MoveTile.cs
+++ MoveTile.cs
@@ -1,7 +1,8 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
-public enum Direction {
+public enum CardinalDirection {
   None = 0,
   North = 0x1,
   NorthEast = 0x2,
@@ -14,14 +15,92 @@ public enum Direction {
   NorthWest = 0x8,
 }
 
+class Position {
+  public int x = 0;
+  public int y = 0;
+
+  public Position(Position p) {
+    this.x = p.x;
+    this.y = p.y;
+  }
+
+  public Position(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+// Commands are verb perform by the player with the perspective of the player
+interface Command {
+}
+
+struct MoveCommand : Command {
+  public CardinalDirection direction;
+
+  public MoveCommand (CardinalDirection direction) {
+    this.direction = direction;
+  }
+}
+
+class Actor {
+  public Guid id;
+  public Position position = new Position(0, 0);
+}
+
+// Actions are verb perform by actors
+interface Mutation {
+}
+
+struct MoveToMutation : Mutation {
+  Actor subject;
+  Position destination;
+
+  public MoveToMutation(Actor subject, Position destination) {
+    this.subject = subject;
+    this.destination = destination;
+  }
+}
+
+struct State {
+  public Guid player_id;
+  public Dictionary<Guid, Actor> actors;
+}
+
+struct Selector {
+  static public Actor selectPlayerActor (State state) {
+    var a = state.actors[state.player_id];
+
+    return a;
+  }
+}
+
+class StateBuilder {
+  public static State build() {
+    var player_id = new Guid();
+
+    var actors = new Dictionary<Guid, Actor>();
+
+    actors.Add(player_id, new Actor {
+      id = player_id,
+    });
+
+    State state = new State {
+      player_id = player_id,
+      actors = actors,
+    };
+
+    return state;
+  }
+}
+
 public class MoveTile : Node2D
 {
   [Export(PropertyHint.Enum)]
-  public Direction direction = Direction.None;
+  public CardinalDirection direction = CardinalDirection.None;
 
-  private ColorRect rect;
+  private ColorRect? rect;
 
-  private Sprite icon;
+  private Sprite? icon;
 
   // Declare member variables here. Examples:
   // private int a = 2;
@@ -39,20 +118,119 @@ public class MoveTile : Node2D
   }
 
   public void _on_ColorRect_mouse_entered () {
-    rect.Color = new Color(1, 0, 1, 0.30f);
-    icon.Visible = true;
+    if (rect != null) {
+      rect.Color = new Color(1, 0, 1, 0.30f);
+    }
+    if (icon != null) {
+      icon.Visible = true;
+    }
     // Input.SetCustomMouseCursor()
   }
 
 
   public void _on_ColorRect_mouse_exited () {
-    rect.Color = new Color(1, 0, 1, 0.12f);
-    icon.Visible = false;
+    if (rect != null) {
+      rect.Color = new Color(1, 0, 1, 0.12f);
+    }
+    if (icon != null) {
+      icon.Visible = false;
+    }
   }
 
+  State state = StateBuilder.build();
+
   public override void _Input (InputEvent @event) {
-    if (@event is InputEventMouseButton && @event.IsPressed() && icon.Visible) {
-      GD.Print(" ==#== ", direction);
+    if (@event is InputEventMouseButton && @event.IsPressed() && icon != null && icon.Visible) {
+      var r = MutationPlaner.plan(new MoveCommand(direction), state);
+
+      if (r.mutations != null) {
+        foreach (var mutation in r.mutations) {
+          GD.Print(mutation);
+        }
+      }
     }
+  }
+}
+
+class MutationPlaner {
+  public interface Error {}
+
+  public struct Result {
+    public Mutation[] mutations;
+    public Error? error;
+
+    public Result(Mutation[] mutations)
+    {
+        this.mutations = mutations;
+        this.error = null;
+    }
+
+    public Result(Error error)
+    {
+        this.mutations = new Mutation[0];
+        this.error = error;
+    }
+  }
+
+  static Result plan (MoveCommand command, State state) {
+    var player = Selector.selectPlayerActor(state);
+
+    var destination = new Position(player.position);
+
+    switch (command.direction) {
+      case CardinalDirection.North:
+        destination.y += 1;
+        break;
+      case CardinalDirection.South:
+        destination.y -= 1;
+        break;
+      case CardinalDirection.East:
+        destination.x += 1;
+        break;
+      case CardinalDirection.West:
+        destination.x -= 1;
+        break;
+    }
+
+    Mutation[] mutations = {
+      new MoveToMutation(player, destination),
+    };
+
+    return new Result(mutations);
+  }
+
+  public static Result plan (Command command, State state) {
+    if (command is MoveCommand) {
+      return plan((MoveCommand)command, state);
+    }
+
+    return new Result {
+      error = null,
+    };
+  }
+}
+
+class MutationApplyer {
+  public interface Error {}
+
+  public struct Result {
+    public State? state;
+    public Error? error;
+
+    public Result(State state)
+    {
+        this.state = state;
+        this.error = null;
+    }
+
+    public Result(Error error)
+    {
+        this.state = null;
+        this.error = error;
+    }
+  }
+
+  public static Result apply (Mutation[] mutations, State state) {
+    return new Result(state);
   }
 }
--- MoveTiles.cs
+++ MoveTiles.cs
@@ -3,23 +3,27 @@ using System;
 
 public class MoveTiles : Node2D
 {
-    private PackedScene tileScene;
+    private PackedScene? tileScene;
 
     public override void _Ready()
     {
       tileScene = GD.Load<PackedScene>("./MoveTile.tscn");
 
-      AddTile(Direction.North, new Vector2(0, -96));
-      AddTile(Direction.NorthEast, new Vector2(96, -96));
-      AddTile(Direction.South, new Vector2(0, 96));
-      AddTile(Direction.SouthEast, new Vector2(96, 96));
-      AddTile(Direction.East, new Vector2(96, 0));
-      AddTile(Direction.SouthWest, new Vector2(-96, 96));
-      AddTile(Direction.West, new Vector2(-96, 0));
-      AddTile(Direction.NorthWest, new Vector2(-96, -96));
+      AddTile(CardinalDirection.North, new Vector2(0, -96));
+      AddTile(CardinalDirection.NorthEast, new Vector2(96, -96));
+      AddTile(CardinalDirection.South, new Vector2(0, 96));
+      AddTile(CardinalDirection.SouthEast, new Vector2(96, 96));
+      AddTile(CardinalDirection.East, new Vector2(96, 0));
+      AddTile(CardinalDirection.SouthWest, new Vector2(-96, 96));
+      AddTile(CardinalDirection.West, new Vector2(-96, 0));
+      AddTile(CardinalDirection.NorthWest, new Vector2(-96, -96));
     }
 
-    private Node AddTile(Direction direction, Vector2 position) {
+    private Node? AddTile(CardinalDirection direction, Vector2 position) {
+      if (tileScene == null) {
+        return null;
+      }
+
       var tile = tileScene.Instance<MoveTile>();
 
       tile.direction = direction;
