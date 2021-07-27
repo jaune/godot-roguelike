c4f6ea7f2272e44ab3fcf79c05395b0d8d82847c
Jaune <jaunelacouleur@gmail.com>
2021-07-27 23:43:00 +0200

    feat: add ChunkedTileMap

---
+++ Maps/Test/basic-tileset.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="basic-tileset" tilewidth="64" tileheight="64" tilecount="9" columns="3" spacing="8">
+ <image source="basic-tileset.png" width="192" height="192"/>
+ <tile id="1">
+  <properties>
+   <property name="walkable:false" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
